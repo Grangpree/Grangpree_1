a295bc7c7f1dd6f374f6ee03266b0e8dfc4e98bc
Minwoo-Kang <sheepbomb@naver.com>
2018-01-29 23:52:07 +0900

    "Edit build.gradle for querydsl"

--- src/main/java/com/granpree/dao/PersonDao.java
+++ src/main/java/com/granpree/dao/PersonDao.java
@@ -1,4 +1,39 @@
 package com.granpree.dao;
 
-public class PersonDao {
+import com.querydsl.jpa.impl.JPAQuery;
+import com.granpree.entity.Person;
+
+import com.granpree.entity.QPerson;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.*;
+
+import java.util.List;
+
+@Repository
+public class  PersonDao{
+
+    private EntityManagerFactory emf = Persistence.createEntityManagerFactory("minwoo");
+    private EntityManager em = emf.createEntityManager();
+    final static QPerson person  = QPerson.person;
+
+
+    public Person save(final Person Person){
+        em.persist(Person);
+        return Person;
+    }
+
+
+    public List<Person> findPersonsByFirstnameAndSurnameQueryDSL(final String firstname, final String surname) {
+        final JPAQuery<Person> query = new JPAQuery<>(em);
+
+        return query.from(person).where(person.firstname.eq(firstname).and(person.surname.eq(surname))).fetch();
+    }
+
+
+    public List<Person> findPersonsByFirstnameQueryDSL(final String firstname){
+        final JPAQuery<Person> query = new JPAQuery<>(em);
+
+        return query.from(person).where(person.firstname.eq(firstname)).fetch();
+    }
 }
--- src/main/java/com/granpree/entity/Person.java
+++ src/main/java/com/granpree/entity/Person.java
@@ -1,4 +1,52 @@
 package com.granpree.entity;
+import javax.persistence.*;
 
+@Entity
+@Table(name = "person")
 public class Person {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String firstname;
+
+    @Column
+    private String surname;
+
+    public Person() {
+    }
+
+    public Person(String firstname, String surname) {
+        this.firstname = firstname;
+        this.surname = surname;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Person setId(Long id) {
+        this.id = id;
+        return this;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public Person setFirstname(String firstname) {
+        this.firstname = firstname;
+        return this;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public Person setSurname(String surname) {
+        this.surname = surname;
+        return this;
+    }
 }
